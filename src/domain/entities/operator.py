4f6a2cdaa1e297106e41fbb7001c43265a45809e
Leonid Laktionov <lglaktionov@edu.hse.ru>
2025-12-30 06:26:07 +0300

    add Operator entity

---
+++ src/domain/entities/operator.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class Operator:
+    id: Optional[int]
+    name: str
+    is_active: bool = True
+    max_load: int = 10
+    current_load: int = 0
+
+    def can_accept_lead(self) -> bool:
+        """Check, the operator can accept new appel"""
+        return self.is_active and self.current_load < self.max_load
---
+++ tests/unit/domain/entities/test_operator.py
@@ -0,0 +1,41 @@
+import pytest
+
+from src.domain.entities.operator import Operator
+
+
+class TestOperator:
+    @pytest.fixture
+    def operator(self) -> Operator:
+        return Operator(id=1, name="Test name")
+
+
+
+    """Tests for entity Operator"""
+    def test_create_operator_with_name(self, operator):
+        """The operator must be created with name"""
+        assert operator.id == 1
+        assert operator.name == "Test name"
+
+    def test_operator_default_values(self, operator):
+        """The operator must be by default is active and have limit 10"""
+        assert operator.is_active is True
+        assert operator.max_load == 10
+        assert operator.current_load == 0
+
+    def test_can_accept_lead_when_active_and_has_capactiry(self, operator):
+        """The active operator without leads may accept lead"""
+        operator.current_load=5
+        assert operator.can_accept_lead() is True
+
+    def test_cannot_accept_lead_when_inactive(self, operator):
+        """The inactive ooperator can't accept lead"""
+        operator.is_active = False
+
+        assert operator.can_accept_lead() is False
+
+    def test_cannot_accept_lead_when_at_capactiy(self, operator):
+        """The operator with an exhausted limit cannot accept a lead"""
+        operator.max_load = 5
+        operator.current_load = 5
+
+        assert operator.can_accept_lead() is False
