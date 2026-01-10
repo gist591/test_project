bc1438ec25f066ef611cb6779bdff98203d13cf4
Leonid Laktionov <lglaktionov@edu.hse.ru>
2026-01-10 21:32:30 +0300

    add repository interfaces

--- src/domain/repositories/__init__.py
+++ src/domain/repositories/__init__.py
@@ -1,10 +1,16 @@
 from .base import BaseRepository
 from .lead_repository import LeadRepository
 from .operator_repository import OperatorRepository
+from .contact_repository import ContactRepository
+from .operator_source_repository import OperatorSourceRepository
+from .source_repository import SourceRepository
 
 
 __all__ = [
     'BaseRepository',
     'LeadRepository',
     'OperatorRepository',
+    'ContactRepository',
+    'OperatorSourceRepository',
+    'SourceRepository',
 ]
